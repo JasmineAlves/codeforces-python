60ad8d8b48902110ebc059a3cef018600dca5f78
JasmineAlves <jascpz.tec@gmail.com>
2025-03-09 15:27:11 -0300

    feat: new commit contests

---
+++ contests/08-03-2025/2_bilhetes_sbctc.py
@@ -0,0 +1,6 @@
+input = int(input())
+
+if (input > 2 and input % 2 == 0):
+    print("YES")
+else:
+    print("NO")
---
+++ contests/08-03-2025/3_codigo_nlogonico.py
@@ -0,0 +1,6 @@
+a = str(input())
+
+num = a.split('+')
+num.sort()
+new_a = '+'.join(num)
+print(new_a)
